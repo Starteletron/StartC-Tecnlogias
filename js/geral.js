57db067f39a04453edb81fb2860ad1b832d7eef9
Charlis_Dias <83874687+Starteletron@users.noreply.github.com>
2025-04-21 22:14:28 -0300

    Create geral.js

---
+++ js/geral.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('menu-toggle');
+    const sidebar = document.getElementById('sidebar');
+  
+    toggleButton.addEventListener('click', () => {
+      sidebar.classList.toggle('active');
+    });
+  });
+  
