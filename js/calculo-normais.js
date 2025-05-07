8b39ecef512760c72e22f99644ce05ed912100d1
Charlis_Dias <83874687+Starteletron@users.noreply.github.com>
2025-05-07 01:23:07 -0300

    Update calculo-normais.js

--- js/calculo-normais.js
+++ js/calculo-normais.js
@@ -39,19 +39,23 @@ async function processarZip() {
       const parser = new DOMParser();
       const xmlDoc = parser.parseFromString(xmlContent, "application/xml");
 
-      // Verificar se os dados existem antes de processar
       const valorNota = getValueFromXML(xmlDoc, 'valotTotalNota')[0]?.textContent;
-      const valorISSNota = getValueFromXML(xmlDoc, 'valorTotalISS')[0]?.textContent;
+
+      // Verifica se o ISS foi retido
+      const tipoRecolhimento = getValueFromXML(xmlDoc, 'tipoRecolhimento')[0]?.textContent;
+      let valorISSRetido = 0;
+      if (tipoRecolhimento === 'RETIDO') {
+        valorISSRetido = parseFloat(getValueFromXML(xmlDoc, 'valorTotalISS')[0]?.textContent) || 0;
+      }
+
       const pis_retido = getValueFromXML(xmlDoc, 'valorImposto')[0]?.textContent;
       const cofins_retido = getValueFromXML(xmlDoc, 'valorImposto')[1]?.textContent;
       const csll_retido = getValueFromXML(xmlDoc, 'valorImposto')[4]?.textContent;
       const irpj_retido = getValueFromXML(xmlDoc, 'valorImposto')[3]?.textContent;
 
-      if (valorNota && valorISSNota && pis_retido && cofins_retido && csll_retido && irpj_retido) {
-        // Acumula os valores
-        // Acumula os valores
+      if (valorNota && pis_retido && cofins_retido && csll_retido && irpj_retido) {
         totalNotas += parseFloat(valorNota) || 0;
-        totalISS += parseFloat(valorISSNota) || 0;
+        totalISS += valorISSRetido;
         totalPIS_Retido += parseFloat(pis_retido) || 0;
         totalCOFINS_Retido += parseFloat(cofins_retido) || 0;
         totalCSLL_Retido += parseFloat(csll_retido) || 0;
@@ -61,18 +65,20 @@ async function processarZip() {
       }
     }
 
-    // Exibe os resultados  
+    // Cálculos apurados
+    const pis_Apurado = totalNotas * 0.65 / 100;
+    const cofins_Apurado = totalNotas * 3 / 100;
+    const csll_Apurado = totalNotas * (32 / 100) * (9 / 100);
+    const irpj_Apurado = totalNotas * (32 / 100) * (15 / 100);
 
-    const pis_Apurado = totalNotas*0.65/100
-    const cofins_Apurado = totalNotas*3/100
-    const csll_Apurado = totalNotas*(32/100)*(9/100)
-    const irpj_Apurado = totalNotas*(32/100)*(15/100)
+    const pis_recolher = pis_Apurado - totalPIS_Retido;
+    const cofins_recolher = cofins_Apurado - totalCOFINS_Retido;
+    const csll_recolher = csll_Apurado - totalCSLL_Retido;
+    const irpj_recolher = irpj_Apurado - totalIRPJ_Retido;
+    const ISS_recolher = (totalNotas * (5 / 100)) - totalISS
 
-    const pis_recolher = pis_Apurado - totalPIS_Retido
-    const cofins_recolher = cofins_Apurado - totalCOFINS_Retido
-    const csll_recolher = csll_Apurado - totalCSLL_Retido
-    const irpj_recolher = irpj_Apurado - totalIRPJ_Retido
 
+    // Exibe os resultados
     document.getElementById('totalNotas').textContent = totalNotas.toFixed(2);
     document.getElementById('totalISS').textContent = totalISS.toFixed(2);
     document.getElementById('totalPIS_Retido').textContent = totalPIS_Retido.toFixed(2);
@@ -80,12 +86,11 @@ async function processarZip() {
     document.getElementById('totalCSLL_Retido').textContent = totalCSLL_Retido.toFixed(2);
     document.getElementById('totalIRPJ_Retido').textContent = totalIRPJ_Retido.toFixed(2);
 
-
+    document.getElementById('totalISS_Recolher').textContent = ISS_recolher.toFixed(2);
     document.getElementById('totalPIS_Recolher').textContent = pis_recolher.toFixed(2);
     document.getElementById('totalCOFINS_Recolher').textContent = cofins_recolher.toFixed(2);
     document.getElementById('totalCSLL_Recolher').textContent = csll_recolher.toFixed(2);
     document.getElementById('totalIRPJ_Recolher').textContent = irpj_recolher.toFixed(2);
-
 
     // Exibe o painel de resultados
     document.getElementById('resultadoImpostos').style.display = 'block';
